fa3ac7d62731fa88584baf06e963265aee53c063
pdoskoch <mergandevinsander@gmail.com>
2018-03-22 13:39:04 +0300

    remove extra libs

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+    app     = express();
     
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
