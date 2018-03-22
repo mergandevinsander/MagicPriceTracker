e2cde6745123a2e60623bbe4d8e8e7799f75a1a0
pdoskoch <mergandevinsander@gmail.com>
2018-03-22 17:21:43 +0300

    build mongo url as in sample

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -2,13 +2,27 @@ var mongoose    = require('mongoose');
 //var log         = require('./log')(module);
 //var config      = require('./config');
 
-var dbName = 'sampledb';
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
-var url = '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
 
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    url = process.env.OPENSHIFT_MONGODB_DB_URL +
-    process.env.OPENSHIFT_APP_NAME;
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
 }
 
 /*mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + dbName;
