acaec955f9aa29c1096a5240343d6bde8908c711
pdoskoch <mergandevinsander@gmail.com>
2018-03-22 13:26:08 +0300

    fix build

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
