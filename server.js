f1907af020d9168491365498f7756cb4a30bc573
pdoskoch <mergandevinsander@gmail.com>
2018-03-22 16:28:40 +0300

    try to fix package. remove direct call index page.

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(serveStatic(path.join(__dirname, "views")));
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.render('index.html');
-});
+});*/
 
 app.get('/pagecount', function (req, res) {
   res.send('{ pageCount: -1 }');
